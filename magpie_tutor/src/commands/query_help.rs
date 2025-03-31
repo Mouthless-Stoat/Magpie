76480b4c75dc07600231f09ede343a9a8b755290
Mouthless Stoat <nphuyanh@outlook.com>
2025-03-31 14:51:32 -0300

    [tutor] Add hint to show_query

--- magpie_tutor/src/commands/query_help.rs
+++ magpie_tutor/src/commands/query_help.rs
@@ -12,18 +12,18 @@ macro_rules! query_help {
 #[poise::command(slash_command)]
 pub async fn show_query(ctx: CmdCtx<'_>) -> Res {
     ctx.say(query_help!(
-        name [n]: "Filter for name";
-        description [d]: "Filter for description";
-        rarity [r]: "Filter for rarity";
-        temple [tp]: "Filter for temple";
-        tribe [tb]: "Filter for tribe";
-        attack [a]: "Filter for attack";
-        health [h]: "Filter for health";
-        sigil [s]: "Filter for sigil";
-        spatk [sp]: "Filter for special attack";
-        cost [c]: "Filter for cost";
-        costtype [ct]: "Filter for cost type";
-        trait [tr]: "Filter for cost"
+        name [n]: "Filter for name.";
+        description [d]: "Filter for description.";
+        rarity [r]: "Filter for rarity. Possible values: `side`, `common`, `rare`, `unique` as well as their shorthand.";
+        temple [tp]: "Filter for temple. Possible values: `beast`, `undead`, `technology`, `fool`, `artistry` as well as their shorthand.";
+        tribe [tb]: "Filter for tribe.";
+        attack [a]: "Filter for attack.";
+        health [h]: "Filter for health.";
+        sigil [s]: "Filter for sigil.";
+        spatk [sp]: "Filter for special attack. Possible values: `mox`, `green`, `mirror`, `ant`, `bone`, `bell`, `card`.";
+        cost [c]: "Filter for cost. Must follow the format of `[amount][type]`. Example `1b` would search for 1 blood, `3b6e2o` would search for 3 blood, 6 energy and 2 bones.";
+        costtype [ct]: "Filter for cost type. Possible values: `b`, `o`, `e`, `m` and any combination of them.";
+        trait [tr]: "Filter for trait."
     ))
     .await?;
 
